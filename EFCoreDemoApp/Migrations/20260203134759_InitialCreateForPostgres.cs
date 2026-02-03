24fa963f67d343aa6f5c47e9bcbd2e526f1c3859
Prasun Thapliyal <prasun.thapliyal@gmail.com>
2026-02-03 19:19:03 +0530

    dotnet ef migrations add InitialCreateForPostgres

---
+++ EFCoreDemoApp/Migrations/20260203134759_InitialCreateForPostgres.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EFCoreDemoApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateForPostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "book_stores",
+                columns: table => new
+                {
+                    book_store_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_book_stores", x => x.book_store_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "authors",
+                columns: table => new
+                {
+                    author_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    book_store_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_authors", x => x.author_id);
+                    table.ForeignKey(
+                        name: "fk_authors_book_stores_book_store_id",
+                        column: x => x.book_store_id,
+                        principalTable: "book_stores",
+                        principalColumn: "book_store_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    book_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "text", nullable: true),
+                    book_store_id = table.Column<int>(type: "integer", nullable: false),
+                    author_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_books", x => x.book_id);
+                    table.ForeignKey(
+                        name: "fk_books_authors_author_id",
+                        column: x => x.author_id,
+                        principalTable: "authors",
+                        principalColumn: "author_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_books_book_stores_book_store_id",
+                        column: x => x.book_store_id,
+                        principalTable: "book_stores",
+                        principalColumn: "book_store_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_authors_book_store_id",
+                table: "authors",
+                column: "book_store_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_books_author_id",
+                table: "books",
+                column: "author_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_books_book_store_id",
+                table: "books",
+                column: "book_store_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "books");
+
+            migrationBuilder.DropTable(
+                name: "authors");
+
+            migrationBuilder.DropTable(
+                name: "book_stores");
+        }
+    }
+}
